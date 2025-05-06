faaab1688a93c9bbf9c99ba51afb30a619d1c436
Sials Detering <silas.detering@tu-dortmund.de>
2025-05-06 22:21:30 +0200

    add initial components and styles for blog, home, gallery, about-me, location, footer, and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { LocationComponent } from './components/location/location.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'über-mich', component: AboutMeComponent },
+  { path: 'einsatzort', component: LocationComponent },
+  { path: 'bilder', component: GalleryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { LocationComponent } from './components/location/location.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    NavbarComponent,
+    GalleryComponent,
+    AboutMeComponent,
+    LocationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
