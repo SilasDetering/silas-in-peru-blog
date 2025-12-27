52a1ee333fecbf692536af66e538b9e2cb4d6d94
silas.detering <silas.detering@yahoo.de>
2025-12-27 11:43:12 -0500

    dezember weitergearbeitet

--- src/app/blog-pages/dez-blog/dez-blog.component.ts
+++ src/app/blog-pages/dez-blog/dez-blog.component.ts
@@ -10,15 +10,17 @@ import { ImagesService } from '../../services/images.service';
 export class DezBlogComponent {
 
   // Bild für das HTML (immer in der HTML festgelegt, hier wird die CDN-URL bereitgestellt)
-  readonly image1Filename = '9C3EC1A0-F138-46A3-BC42-6CEB0F9457BF_1_105_c.jpeg';
-  readonly image2Filename = 'A770E0B8-8871-427F-B30E-921DD2E8A0DA.JPG';
-  readonly image3Filename = 'IMG_1031.JPG';
-  readonly image4Filename = 'IMG_1214.jpeg';
+  readonly image1Filename = 'IMG_1459.JPG';
+  readonly image2Filename = 'IMG_2919.JPG';
+  readonly image3Filename = 'B3188E6D-32D6-43AF-BF0A-9E3E144C3747.JPG';
+  readonly image4Filename = '';
+  readonly image5Filename = '';
 
   image1: string = '';
   image2: string = '';
   image3: string = '';
   image4: string = '';
+  image5: string = '';
 
   constructor(private sanitizer: DomSanitizer, private imagesService: ImagesService) {
     this.image1 = this.imagesService.getCdnSmallUrl(this.image1Filename);
--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -2,6 +2,7 @@ import { Component, Type } from '@angular/core';
 import { Sept20BlogComponent } from '../../blog-pages/sept-blog/sept-20-blog.component';
 import { Okt20BlogComponent } from '../../blog-pages/okt-blog/okt-20-blog.component';
 import { NovBlogComponent } from '../../blog-pages/nov-blog/nov-blog.component';
+import { DezBlogComponent } from '../../blog-pages/dez-blog/dez-blog.component';
 
 @Component({
   selector: 'app-blog',
@@ -12,6 +13,7 @@ export class BlogComponent {
 
   blogComponents: Type<any>[] = [
     // weitere Komponenten hier ergänzen, neueste zuerst
+    DezBlogComponent,
     NovBlogComponent,
     Okt20BlogComponent,
     Sept20BlogComponent,
